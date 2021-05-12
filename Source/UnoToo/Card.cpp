ea39d661b69f181811d2d11dba1c019ee63d7b2e
Dylan Loe <dylanloe@gmail.com>
2021-05-12 13:08:56 -0700

    Revert "Merge branch 'Dylan's_Branch' into main"
    This reverts commit f9751903983db381fcc04ebaa09c29d014eaae6f, reversing
    changes made to 9dbebe72e92494f7c8111a6a0b36a62c59f07d07.

--- Source/UnoToo/Card.cpp
+++ Source/UnoToo/Card.cpp
@@ -2,25 +2,20 @@
 
 
 #include "Card.h"
-#include "Blueprint/UserWidget.h"
 
 // Sets default values
 ACard::ACard()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = false;
+	PrimaryActorTick.bCanEverTick = true;
 
-	isSpecialCard = false;
-	cardNumber = -1;
-	cardColor = ECardColor::None;
-	cardType = ESpecialCardType::None;
 }
 
 // Called when the game starts or when spawned
 void ACard::BeginPlay()
 {
 	Super::BeginPlay();
-
+	
 }
 
 // Called every frame
